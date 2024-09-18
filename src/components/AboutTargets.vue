904fa44b2f9b4cb9eefbd0b9377262be6a3661e5
Salah <altigov00@mail.ru>
2024-09-18 16:43:29 +0300

    feat add AboutPage and components

---
+++ src/components/AboutTargets.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="about-targets about-container row justify-center items-center">
+    <div class="about-title about-targets__title">Наши цели</div>
+    <div class="row q-mt-lg justify-center items-center">
+      <div class="col-7">
+        <p class="about-text">Выбрав этот кейс примерно две недели назад перед командой сразу встал серьезный вопрос - <span class="about-span">“Для кого?”</span></p>
+        <br>
+        <p class="about-text">Кажется, что ответ вполне очевиден, однако среди <span class="about-span"> экологического сообщества </span> так же сущесвует множество других <span class="about-span"> социальных групп </span>, где <span class="about-span">вкусы</span> могут значительно различаться</p>
+      </div>
+      <div class="col-5 row justify-center items-center">
+        <q-img :src="targetsFirst" width="500px" height="540px"></q-img>
+      </div>
+    </div>
+    <div class="row q-mt-lg justify-center items-center">
+      <div class="col-5 row justify-center items-center">
+        <q-img :src="targetsSecond" width="500px" height="500px"></q-img>
+      </div>
+      <div class="col-7">
+        <p class="about-text">После недолгих раздумий, наша команда дала максимально <span class="about-span">простой ответ</span>, который уже менее очевиден</p>
+        <p class="about-span text-h2 text-right q-my-md"> - Для всех!</p>
+        <p class="about-text">Именно поэтому данный прототип способен заинтересовать все подгруппы <span class="about-span">одного большого сообщества!</span></p>
+      </div>
+    </div>
+    <div class="about-title">То, что нас объединяет</div>
+  </div>
+  <div class="wrap">
+      <div class="items-wrap">
+        <div class="items marquee">
+          <div class="item">Система уровней</div>
+          <div class="item mt-xl">Бонусная программа</div>
+          <div class="item">Собственный бот</div>
+          <div class="item mt-xl">Современный дизайн</div>
+          <div class="item">Авторский блог</div>
+          <div class="item mt-xl">Свежие новости</div>
+        </div>
+        <div aria-hidden="true" class="items marquee">
+          <div class="item">Система уровней</div>
+          <div class="item mt-xl">Бонусная программа</div>
+          <div class="item">Собственный бот</div>
+          <div class="item mt-xl">Современный дизайн</div>
+          <div class="item">Авторский блог</div>
+          <div class="item mt-xl">Свежие новости</div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import targetsFirst from 'images/targets-first.svg'
+import targetsSecond from 'images/targets-second.svg'
+import targetsTitleBack from 'images/targets-title-back.svg'
+</script>
--- src/pages/about-us/AboutPage.vue
+++ src/pages/about-us/AboutPage.vue
@@ -1,7 +1,25 @@
 <template>
-    <q-page class="about-page">
-        <h1>about</h1>
+    <q-page class="about-page column justify-center items-center">
+      <q-img class="absolute-left" :src="foliageLeft" height="360px" width="540px"/>
+      <q-img class="absolute-right" :src="foliageRight" height="360px" width="540px"/>
+      <div class="about-page__banner column justify-center items-center q-mt-xl">
+        <p class="about-page__title">Мы - команда <span class="about-page__title-span">template</span></p>
+        <p class="about-page__under-title">Данная страница является ознакомительной и <br> не несет в себе функциональной нагрузки</p>
+        <q-img :src="aboutMain" width="1000px" height="630"></q-img>
+      </div>
     </q-page>
+    <div class="column justify-center items-center">
+      <AboutTargets />
+      <AboutProblems />
+      <AboutAdditionaly />
+    </div>
   </template>
-  
-  <script setup lang="ts"></script>
+
+<script setup lang="ts">
+import foliageLeft from "images/foliage-left.svg"
+import foliageRight from "images/foliage-right.svg"
+import aboutMain from "images/about-main.svg"
+import AboutTargets from "src/components/AboutTargets.vue";
+import AboutProblems from "src/components/AboutProblems.vue";
+import AboutAdditionaly from "src/components/AboutAdditionaly.vue";
+</script>
