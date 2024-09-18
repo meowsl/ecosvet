7c290e22fd776efb12fa7a6b85d691e8851cbf14
meowsl <vlafrank@ya.ru>
2024-09-18 23:50:42 +0300

    fix: flexs and links for contacts

--- src/components/AboutFooter.vue
+++ src/components/AboutFooter.vue
@@ -2,15 +2,42 @@
   <div class="about-footer row justify-between q-pa-lg">
     <div class="">
       <p class="q-mb-md">Разработано с душой и любовью <br>
-      командой <span class="text-weight-regular">template</span></p>
+        командой <span class="text-weight-regular">template</span></p>
       <p>© 2024</p>
     </div>
     <div class="">
       <p class="q-mb-md">Контакты команды</p>
       <div class="text-right q-gutter-sm">
-        <q-img :src="FigmaIcon" width="30px" height="30px"></q-img>
-        <q-img :src="GitHubIcon" width="30px" height="30px"></q-img>
-        <q-img :src="TgIcon" width="30px" height="30px"></q-img>
+        <a
+          target="_blank"
+          href="https://www.figma.com/design/wiotFXVsLsShy6vIbojjG3/%D0%AD%D0%9A%D0%9E%D0%A1%D0%B2%D0%B5%D1%82?node-id=0-1&t=N4YP2C0YOVa1EdyN-1"
+        >
+          <q-img
+            :src="FigmaIcon"
+            width="30px"
+            height="30px"
+          />
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/meowsl/ecosvet"
+        >
+          <q-img
+            :src="GitHubIcon"
+            width="30px"
+            height="30px"
+          ></q-img>
+        </a>
+        <a
+          target="_blank"
+          href="https://t.me/slay_meow"
+        >
+          <q-img
+            :src="TgIcon"
+            width="30px"
+            height="30px"
+          ></q-img>
+        </a>
       </div>
     </div>
   </div>
