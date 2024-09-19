c73ff5a9d3547e78db7b77fee126c57f9a31b337
Oizag <visolanadolp@gmail.com>
2024-09-19 10:32:24 +0300

    feat: create achieve for admin

--- server/apps/api/reference/v1/views/achievement.py
+++ server/apps/api/reference/v1/views/achievement.py
@@ -1,7 +1,13 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
 from apps.api.reference.models import Achievement
 from apps.api.reference.v1.serializers import AchievementSerializer
 
 class AchievementAPIListView(generics.ListAPIView):
     queryset = Achievement.objects.all()
     serializer_class = AchievementSerializer
+
+class AchievementCreateView(generics.CreateAPIView):
+    queryset = Achievement.objects.all()
+    serializer_class = AchievementSerializer
+    permission_classes = (IsAdminUser,)
